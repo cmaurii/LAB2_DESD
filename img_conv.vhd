1aef553772769adff4d901731695bfd4dbe35f7a
zixand <ziggiottiandrea1@gmail.com>
2025-04-18 00:58:24 +0200

    Fix convoluzione

--- img_conv.vhd
+++ img_conv.vhd
@@ -4,8 +4,8 @@ use ieee.numeric_std.all;
 
 entity img_conv is
     generic(
-        LOG2_N_COLS: POSITIVE :=8;
-        LOG2_N_ROWS: POSITIVE :=8
+        LOG2_N_COLS: POSITIVE := 8;
+        LOG2_N_ROWS: POSITIVE := 8
     );
     port (
 
@@ -40,21 +40,27 @@ architecture rtl of img_conv is
     signal is_pixel_valid : std_logic := '0';
     signal is_pixel_done : std_logic := '0';
     signal is_image_done : std_logic := '0';
-    signal real_pixel_address : integer range 0 to 255*255 := 0;
+    signal real_pixel_address : integer range 0 to ((2**LOG2_N_COLS) * (2**LOG2_N_COLS))-1 := 0;
     signal reset_sum : std_logic := '0';
 
     type state_type is (WAIT_START, CONVERTING, SEND_PIXEL, SEND_FINSHED);
     signal curr_state: state_type := WAIT_START; 
-
+    
 begin
 
     m_axis_tvalid <= m_axis_tvalid_int; 
     
     -- FSM
-    process(clk)
+    process(clk, aresetn)
     begin
-    
-        if rising_edge(clk) then
+        if aresetn = '0' then
+            reset_sum <= '0';
+            done_conv <= '0';
+            reset_sum <= '0';
+            done_conv <= '0';
+            m_axis_tlast <= '0';
+            is_counter_enabled <= '0';
+        elsif rising_edge(clk) then
             reset_sum <= '0';
             done_conv <= '0';
             m_axis_tlast <= '0';
@@ -78,9 +84,11 @@ begin
         end if;
     end process;
 
-    process(clk)
+    process(clk, aresetn)
     begin
-        if rising_edge(clk) then
+        if aresetn = '0' then
+            curr_state <= WAIT_START;
+        elsif rising_edge(clk) then
             case curr_state is
                 when WAIT_START =>
                     if start_conv = '1' then
@@ -102,9 +110,17 @@ begin
 
 
     -- Offset counter
-    process(clk)
+    process(clk, aresetn)
     begin
-        if rising_edge(clk) then
+        if aresetn = '0' then
+            is_pixel_done <= '0';
+            is_image_done <= '0';
+            col <= 0;
+            raw <= 0;
+            conv_addr <= (others => '0');
+            real_pixel_address <= 0;
+            is_pixel_valid <= '0';
+        elsif rising_edge(clk) then
             -- Reset Logic bit
             is_pixel_done <= '0';
             is_image_done <= '0';
@@ -127,10 +143,10 @@ begin
                 end if;
 
                 -- Check if the new address is valid and set it
-                if ((real_pixel_address mod 256) + (col - 1) >= 0 and  ((real_pixel_address mod 256) + (col - 1) <= 255)) then
-                    if((real_pixel_address / 256) + (raw - 1) >= 0 and (real_pixel_address / 256 + (raw - 1) <= 255)) then
+                if ((real_pixel_address mod (2**LOG2_N_COLS)) + (col - 1) >= 0 and  ((real_pixel_address mod (2**LOG2_N_COLS)) + (col - 1) <= (2**LOG2_N_COLS - 1))) then
+                    if((real_pixel_address / (2**LOG2_N_ROWS)) + (raw - 1) >= 0 and (real_pixel_address / (2**LOG2_N_ROWS) + (raw - 1) <= (2**LOG2_N_ROWS - 1))) then
                         is_pixel_valid <= '1';
-                        conv_addr <= std_logic_vector(to_unsigned((real_pixel_address + (col - 1) + ((raw - 1) * 256)), conv_addr'length));
+                        conv_addr <= std_logic_vector(to_unsigned((real_pixel_address + (col - 1) + ((raw - 1) * (2**LOG2_N_COLS))), conv_addr'length));
                     else
                         is_pixel_valid <= '0';
                     end if;
@@ -139,24 +155,29 @@ begin
                 end if;
 
                 -- Check if the image is done
-                if (real_pixel_address = 255*255) and (raw = 2) and (col = 2) then
+                if (real_pixel_address = (((2**LOG2_N_COLS) * (2**LOG2_N_COLS))-1)) and (raw = 2) and (col = 2) then
                     is_image_done <= '1';
                 end if;
             else
                 conv_addr <= std_logic_vector(to_unsigned(0, conv_addr'length));
                 real_pixel_address <= 0;
                 raw <= 0;
-                col <= -1;
+                col <= 0;
                 is_pixel_valid <= '0';
             end if;
         end if;
     end process;
 
     -- SUM anc Check
-    process(clk)
+    process(clk, aresetn)
     variable conv_sum : integer range -1024 to 1024 := 0;
     begin
-        if rising_edge(clk) then
+        if aresetn = '0' then
+            is_data_to_send <= '0';
+            conv_sum := 0;
+            m_axis_tdata <= (others => '0');
+        elsif rising_edge(clk) then
+            is_data_to_send <= '0';
             if reset_sum = '1' then
                 -- Send logic
                 if is_address_valid = '1' then 
@@ -190,7 +211,7 @@ begin
 
             if(m_axis_tready = '1') then
                 m_axis_tvalid_int <= '0';
-                is_data_to_send <= '0';
+                --is_data_to_send <= '0';
                 --m_axis_tlast <= '0';
             end if;
             
